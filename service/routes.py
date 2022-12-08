86fa1c4d861da71d4652a0fde03f11b8e233546f
ryoung441 <114686248+ryoung441@users.noreply.github.com>
2022-12-08 07:23:37 -0600

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -63,7 +63,7 @@ def create_accounts():
 ######################################################################
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
-        """
+    """
     List all Accounts
     This endpoint will list all Accounts
     """
